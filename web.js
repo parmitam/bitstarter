53d627bf8153626662365595a88a47509672f318
Parmita Mehta <parmita.mehta@gmail.com>
2013-07-06 06:54:19 -0700

    added index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
 
+var express = require('express');
+var fs= require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World to Uma!');
+   var data = fs.readFileSync("index.html", "utf8");
+     response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
